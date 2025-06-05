071ddcac0781e5e542102e8d4da48f9ffa81d5b6
Babita Kanojiya <babitakanojiya98@gmail.com>
2025-06-05 16:58:55 +0530

    loadder add in otp page

--- KMI.FRMWRK.Web/Application/CKYC/DownloadOTPPage.aspx.designer.cs
+++ KMI.FRMWRK.Web/Application/CKYC/DownloadOTPPage.aspx.designer.cs
@@ -51,15 +51,6 @@ namespace KMI.FRMWRK.Web.Application.CKYC
         protected global::System.Web.UI.WebControls.TextBox TextBox7;
 
         /// <summary>
-        /// RegexOtp control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegexOtp;
-
-        /// <summary>
         /// btnqcpageopen control.
         /// </summary>
         /// <remarks>
